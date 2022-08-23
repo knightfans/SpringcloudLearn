bf271776d0959e613f7689c48682bc71fdd52d4d
knightfans <2504003605@qq.com>
2022-08-23 17:33:32 +0800

    getWay断言和时间生成

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author lis1
+ * @date 2022/8/23
+ * @time 17:14
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
